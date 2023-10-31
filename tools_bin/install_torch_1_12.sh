5855f6fb49adfe804c2cee9a5d6131bf82090306
dkorchevgithub <dkorchev@gmail.com>
2023-10-30 23:44:54 -0700

    updated torch build

--- tools_bin/install_torch_1_12.sh
+++ tools_bin/install_torch_1_12.sh
@@ -4,6 +4,7 @@
 # upgrade pytorch
 sudo pip3 uninstall -y torch torchvision
 pip3 uninstall -y torch torchvision
+pip3 uninstall -y torchtext
 pip3 install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0 --extra-index-url https://download.pytorch.org/whl/cu113
 
 # re-make Deformable Attention
